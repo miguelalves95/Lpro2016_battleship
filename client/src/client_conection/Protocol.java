c53c08a9a5384da31c99892d8ba456b20327d87a
Inez <inez.spiralska@globegroup.pl>
2016-11-21 17:51:20 +0000

    komit

--- client/src/client_conection/Protocol.java
+++ client/src/client_conection/Protocol.java
@@ -11,8 +11,7 @@ import client_bl.*;
  */
 public class Protocol {
     
-     String gggggg;
-     private shit; 
+    asdfghjkl;lkjhgfdsasdfghjklTEST
     private static Sockets sc;
     private static boolean connected = false;
     
